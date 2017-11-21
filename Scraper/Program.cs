1c740c5492f56a7a9a303d4512cba57b4f304dfc
exbuzz <exbuzz@yahoo.com>
2017-11-21 11:58:48 -0500

    Got HTML all cleaned up

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -42,20 +42,58 @@ namespace Scraper
 
             html = html.Replace("<span class=\"br_italiano\">", string.Empty);
             html = html.Replace("<span lang=\"it\">", string.Empty);
+            html = html.Replace("&nbsp;", string.Empty);
 
-             MemoryStream ms = new MemoryStream();
+
+            html = Regex.Replace(html, @"<([^/]*?)\s+[^/>]+>", "<$1>",RegexOptions.Singleline);
+            html = Regex.Replace(html, @"<!.*?>", string.Empty, RegexOptions.Singleline);
+
+            SanitizedMarkup markup = Sanitizer.SanitizeMarkup(html);
+            html = markup.MarkupText;
+
+            html = html.Replace("--&gt;",string.Empty);
+            html = html.Replace("&gt;", string.Empty);
+
+            html = Regex.Replace(html, @"\&\#[0-9]+\;", string.Empty,RegexOptions.Singleline);
+
+            html = Regex.Replace(html, @"<td>\s*</td>", string.Empty, RegexOptions.Singleline);
+            html = Regex.Replace(html, @"<tr>\s*</tr>", string.Empty, RegexOptions.Singleline);
+            html = Regex.Replace(html, @"<table>\s*</table>", string.Empty, RegexOptions.Singleline);
+            html = Regex.Replace(html, @"<li>\s*</li>", string.Empty, RegexOptions.Singleline);
+            html = Regex.Replace(html, @"<ul>\s*</ul>", string.Empty, RegexOptions.Singleline);
+
+            html = html.Replace("<span>", String.Empty).Replace("</span>",string.Empty);
+            html = html.Replace("<strong>", String.Empty).Replace("</strong>", string.Empty);
+            html = html.Replace("<h3>", String.Empty).Replace("</h3>", string.Empty);
+            html = html.Replace("<p>", String.Empty).Replace("</p>", string.Empty);
+
+            html = "<html><body>" + html + "</body></html>";
+            html = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>" + Environment.NewLine + html;
+
+            MemoryStream ms = new MemoryStream();
             StreamWriter sw = new StreamWriter(ms);
             sw.Write(html);
+            sw.Flush();
             ms.Position = 0;
+            //StreamReader sr = new StreamReader(ms);
+            //string html2 = sr.ReadToEnd();
+
+            //XmlDocument xmlDocument = new XmlDocument();
+            //xmlDocument.LoadXml(html);
 
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.DtdProcessing = DtdProcessing.Ignore;
+            StringBuilder sb = new StringBuilder();
             using (XmlReader reader = XmlReader.Create(ms, settings))
             {
-                reader.MoveToContent();
-                reader.ReadToDescendant("table");
-
-                string x = reader.ReadElementContentAsString();
+                while(reader.Read())
+                {
+                    if(reader.NodeType==XmlNodeType.Element && reader.Name=="td")
+                    {
+                        string x = reader.ReadElementContentAsString();
+                        sb.Append(x);
+                    }
+                }
             }
         }
 
