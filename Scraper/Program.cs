c0066060e992787f9a45f95635b270282bb5c9f6
exbuzz <exbuzz@yahoo.com>
2017-11-20 22:03:23 -0500

    progress

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -21,18 +21,49 @@ namespace Scraper
 
             string html = getHtml(word);
 
-            SanitizedMarkup markup = Sanitizer.SanitizeMarkup(html);
+            //SanitizedMarkup markup = Sanitizer.SanitizeMarkup(html);
+            //html = markup.MarkupText;
 
-            //XmlReaderSettings settings = new XmlReaderSettings();
-            //settings.DtdProcessing = DtdProcessing.Ignore;
-            //using (XmlReader reader = XmlReader.Create(dataStream, settings))
-            //{
-            //    reader.MoveToContent();
-            //    reader.ReadToDescendant("table");
-            //    string x = reader.ReadElementContentAsString();
-            //}
+
+
+            html = removeElement("<script.*?>.*?</script>",html);
+            html = removeElement("<head.*?>.*?</head>", html);
+            html = removeElement("<img.*?>", html);
+            html = removeElement("<br.*?>", html);
+            html = removeElement("<hr.*?>", html);
+            html = removeElement("<input.*?>", html);
+            html = removeElement("<center.*?>.*?</center>", html);
+            html = removeElement("<a.*?>.*?</a>", html);
+            html = html.Replace("<tr></table>", "</tr></table>");
+            html = html.Replace("<span class=\"br_italiano\">", string.Empty);
+            html = html.Replace("<span class=\"br_italiano\" style=\"color:#FF0000; font-weight:bold;\">", string.Empty);
+            html = html.Replace("<td align=\"left\" width=\"50%\"></span></td></tr>", "<td></td></tr>");
+
+
+            html = html.Replace("<span class=\"br_italiano\">", string.Empty);
+            html = html.Replace("<span lang=\"it\">", string.Empty);
+
+             MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms);
+            sw.Write(html);
+            ms.Position = 0;
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Ignore;
+            using (XmlReader reader = XmlReader.Create(ms, settings))
+            {
+                reader.MoveToContent();
+                reader.ReadToDescendant("table");
+
+                string x = reader.ReadElementContentAsString();
+            }
         }
 
+        static string removeElement(string pattern, string html)
+        {
+            Regex regex = new Regex(pattern, RegexOptions.Singleline);
+            return  regex.Replace(html, string.Empty);
+        }
         static string getHtml(string word)
         {
             
