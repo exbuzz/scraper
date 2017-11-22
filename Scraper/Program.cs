4d5fd80f85902722282dd31e267f46e130ccf93e
exbuzz <exbuzz@yahoo.com>
2017-11-21 19:06:00 -0500

    Almost done

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -42,12 +42,153 @@ namespace Scraper
             "TRAPASSATO"
         };
 
+        static string[] getInfinitives()
+        {
+            string file = Path.Combine(Environment.CurrentDirectory,  "words.txt");
+            return File.ReadAllLines(file);
+        }
+
         static void Main(string[] args)
         {
-            string word = "portare";
+            string[] infinitives = getInfinitives();
+            string outFile = Path.Combine(Environment.CurrentDirectory, "out.csv");
+            initializeOutfile(outFile);
 
-            string html = getHtml(word);
+            foreach (string infinitive in infinitives)
+            {
 
+                Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@");
+                Console.WriteLine($"Starting new infinitive '{infinitive}'.");
+                List<string> lines = getDataForInfinitive(infinitive);
+                Console.WriteLine($"Finished processing all forms for '{infinitive}'.");
+
+
+                lines.RemoveAt(0);
+                lines.RemoveAt(0);
+
+                lines.RemoveAt(lines.Count - 1);
+                lines.RemoveAt(lines.Count - 1);
+
+                StreamWriter fileWriter = new StreamWriter(File.Open(outFile,FileMode.Append));
+                foreach (string line in lines)
+                {
+                    fileWriter.WriteLine(line);
+                }
+                fileWriter.Flush();
+                fileWriter.Close();
+                Console.WriteLine($"Finished '{infinitive}'.");
+            }
+            Console.WriteLine("Done.");
+        }
+
+        static void initializeOutfile(string outFile)
+        {
+            StreamWriter fileWriter = File.CreateText(outFile);
+            fileWriter.Flush();
+            fileWriter.Close();
+        }
+        static List<string> getDataForInfinitive(string infinitive)
+        {
+            string englishInfinitive = getEnglish(infinitive);
+
+            string html = getHtml(infinitive);
+            html = sanitize(html);
+
+
+            string currentMode = string.Empty;
+            string currentTense = string.Empty;
+            string currentPronoun = string.Empty;
+
+            int imperativoCounter = 0;
+
+            List<string> lines = new List<string>();
+            using (XmlReader reader = getReader(html))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "td")
+                    {
+                        string currentCell = reader.ReadElementContentAsString();
+                        currentCell = currentCell.Trim();
+                        currentCell = currentCell.ToLower();
+
+                        if (Moods.Contains(currentCell, StringComparer.InvariantCultureIgnoreCase))
+                        {
+                            currentMode = currentCell;
+                            currentTense = string.Empty;
+                            currentPronoun = string.Empty;
+                            Console.WriteLine("#####################");
+                            Console.WriteLine($"Starting mood '{currentMode}'...");
+                            continue;
+                        }
+                        if (Tenses.Contains(currentCell, StringComparer.InvariantCultureIgnoreCase))
+                        {
+                            currentTense = currentCell;
+                            currentPronoun = string.Empty;
+                            Console.WriteLine("---------------------");
+                            Console.WriteLine($"Starting tense '{currentTense}'...");
+                            continue;
+                        }
+
+                        Match pronounMatch = Regex.Match(currentCell, "(io|tu|lui/lei|noi|voi|loro)");
+                        if (pronounMatch.Success)
+                        {
+                            currentPronoun = pronounMatch.Value;
+                        }
+
+                        if (currentMode.Equals("IMPERATIVO", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            handleImperativo(ref imperativoCounter, ref currentPronoun);
+                        }
+                        if (currentCell.Contains(":"))
+                        {
+                            string[] parts = currentCell.Split(':');
+                            currentTense = parts[0]?.Trim();
+                            currentCell = parts[1]?.Trim();
+                        }
+
+
+                        string english = getEnglish(currentCell);
+
+                        lines.Add($"{infinitive},{englishInfinitive},{currentMode},{currentTense},{currentPronoun},{currentCell},{english}");
+                    }
+                }
+            }
+
+            return lines;
+        }
+
+        static XmlReader getReader(string html)
+        {
+
+            MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms);
+            sw.Write(html);
+            sw.Flush();
+            ms.Position = 0;
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Ignore;
+            XmlReader reader = XmlReader.Create(ms, settings);
+            return reader;
+        }
+
+        static void handleImperativo(ref int counter, ref string currentPronoun)
+        {
+            counter++;
+            if (counter == 1) currentPronoun = "tu";
+            if (counter == 2) currentPronoun = "lui/lei";
+            if (counter == 3) currentPronoun = "noi";
+            if (counter == 4) currentPronoun = "voi";
+            if (counter == 5)
+            {
+                currentPronoun = "loro";
+                counter = 0;
+            }
+        }
+
+        static string sanitize(string html)
+        {
             html = removeElement("<script.*?>.*?</script>", html);
             html = removeElement("<head.*?>.*?</head>", html);
             html = removeElement("<img.*?>", html);
@@ -91,110 +232,7 @@ namespace Scraper
 
             html = "<html><body>" + html + "</body></html>";
             html = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>" + Environment.NewLine + html;
-
-            MemoryStream ms = new MemoryStream();
-            StreamWriter sw = new StreamWriter(ms);
-            sw.Write(html);
-            sw.Flush();
-            ms.Position = 0;
-
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.DtdProcessing = DtdProcessing.Ignore;
-            string currentMode = string.Empty;
-            string currentTense = string.Empty;
-            string currentPronoun = string.Empty;
-
-            //List<Tuple<string, string, string, string>> tuples = new List<Tuple<string, string, string, string>>();
-
-            int counter = 0;
-
-            List<string> lines = new List<string>();
-            using (XmlReader reader = XmlReader.Create(ms, settings))
-            {
-                while (reader.Read())
-                {
-                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "td")
-                    {
-                        string currentCell = reader.ReadElementContentAsString();
-                        currentCell = currentCell.Trim();
-                        currentCell = currentCell.ToLower();
-
-                        if (Moods.Contains(currentCell, StringComparer.InvariantCultureIgnoreCase))
-                        {
-                            currentMode = currentCell;
-                            currentTense = string.Empty;
-                            currentPronoun = string.Empty;
-                            Console.WriteLine("#####################");
-                            Console.WriteLine($"Starting mood '{currentMode}'...");
-                            continue;
-                        }
-                        if (Tenses.Contains(currentCell, StringComparer.InvariantCultureIgnoreCase))
-                        {
-                            currentTense = currentCell;
-                            currentPronoun = string.Empty;
-                            Console.WriteLine("---------------------");
-                            Console.WriteLine($"Starting tense '{currentTense}'...");
-                            continue;
-                        }
-
-                        Match pronounMatch = Regex.Match(currentCell, "(io|tu|lui/lei|noi|voi|loro)");
-                        if (pronounMatch.Success)
-                        {
-                            currentPronoun = pronounMatch.Value;
-                        }
-
-                        if (currentMode.Equals("IMPERATIVO", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            counter++;
-                            if (counter == 1) currentPronoun = "tu";
-                            if (counter == 2) currentPronoun = "lui/lei";
-                            if (counter == 3) currentPronoun = "noi";
-                            if (counter == 4) currentPronoun = "voi";
-                            if (counter == 5)
-                            {
-                                currentPronoun = "loro";
-                                counter = 0;
-                            }
-                        }
-                        if (currentCell.Contains(":"))
-                        {
-                            string[] parts = currentCell.Split(':');
-                            currentTense = parts[0];
-                            currentCell = parts[1];
-                        }
-
-                        currentCell = currentCell.Trim();
-
-                        string translateText = currentCell;
-                        string english = string.Empty;
-                        if (currentPronoun == "lui/lei")
-                        {
-                            translateText = translateText.Replace("lei", string.Empty);
-                            translateText = translateText.Replace("/", string.Empty);
-                        }
-                        english = getEnglish(translateText);
-
-                        lines.Add($"{word},{currentMode},{currentTense},{currentPronoun},{currentCell},{english}");
-                    }
-                }
-            }
-
-            Console.WriteLine("Finished all forms.");
-
-            lines.RemoveAt(0);
-            lines.RemoveAt(0);
-
-            lines.RemoveAt(lines.Count - 1);
-            lines.RemoveAt(lines.Count - 1);
-
-            string file = Path.Combine(Environment.CurrentDirectory, word + ".txt");
-            StreamWriter fileWriter = File.CreateText(file);
-            foreach (string line in lines)
-            {
-                fileWriter.WriteLine(line);
-            }
-            fileWriter.Flush();
-            fileWriter.Close();
+            return html;
         }
 
         static string removeElement(string pattern, string html)
@@ -223,9 +261,16 @@ namespace Scraper
             return responseFromServer;
         }
 
-        static string getEnglish(string word)
+        static string getEnglish(string input)
         {
-            Console.Write($"Translating '{word}'...");
+            string translateText = input;
+
+            if (translateText.Contains("lui/lei"))
+            {
+                translateText = translateText.Replace("lei", string.Empty);
+                translateText = translateText.Replace("/", string.Empty);
+            }
+            Console.Write($"Translating '{translateText}'...");
             WebRequest request = WebRequest.Create($"{GoogleTranslateUrl}");
             request.Method = "POST";
             request.ContentType = "application/json";
@@ -233,7 +278,7 @@ namespace Scraper
             StreamWriter bodyWriter = new StreamWriter(request.GetRequestStream());
             var requestObject = new
             {
-                q = new string[] { word },
+                q = new string[] { translateText },
                 source = "it",
                 target = "en"
             };
